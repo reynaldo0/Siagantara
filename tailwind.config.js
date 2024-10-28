13021560824ce76e88f98c25c6e22c16753cb68e
reynaldo0 <reynaldooyusellino@gmail.com>
2024-10-28 12:20:00 +0700

    upd : hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,34 +6,35 @@ export default {
   ],
   theme: {
     extend: {
-      textStroke: {
-        DEFAULT: '1px white',
-        sm: '0.5px white',
-        md: '2px white',
-        lg: '3px white',
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
       },
     },
   },
   plugins: [
     function ({ addUtilities }) {
       addUtilities({
-        '.text-stroke-white': {
+        '.text-stroke': {
           '-webkit-text-stroke': '1px white',
-        },
-        '.text-stroke-sm-white': {
-          '-webkit-text-stroke': '0.5px white',
-        },
-        '.text-stroke-md-white': {
-          '-webkit-text-stroke': '2px white',
-        },
-        '.text-stroke-lg-white': {
-          '-webkit-text-stroke': '3px white',
-        },
-        '.text-fill-transparent': {
           '-webkit-text-fill-color': 'transparent',
+          'color': 'transparent', // Pastikan warna teks utama juga transparan
+        },
+        '.text-stroke-sm': {
+          '-webkit-text-stroke': '0.5px white',
+          '-webkit-text-fill-color': 'transparent',
+          'color': 'transparent',
+        },
+        '.text-stroke-md': {
+          '-webkit-text-stroke': '2px white',
+          '-webkit-text-fill-color': 'transparent',
+          'color': 'transparent',
+        },
+        '.text-stroke-lg': {
+          '-webkit-text-stroke': '3px white',
+          '-webkit-text-fill-color': 'transparent',
+          'color': 'transparent',
         },
       });
     },
   ],
 }
-
