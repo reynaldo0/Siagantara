227a8696cbcc216567be4ab32497cf27c330cd48
reynaldo0 <reynaldooyusellino@gmail.com>
2024-10-28 12:49:29 +0700

    fix : sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,26 +14,26 @@ const Sidebar = () => {
     <div className="relative">
       {/* Button to toggle the sidebar on both mobile and desktop */}
       <button
-        className={`fixed top-20 left-4 z-[999999] bg-[#1E3A5F] text-white p-3 rounded-full md:block transition-all duration-300 ease-in-out mt-2 ${
-          isOpen ? "ml-56 md:ml-[213px]" : "ml-0"
+        className={`fixed top-80 right-4 z-[999999] bg-[#1E3A5F] text-white p-3 rounded-full md:block transition-all duration-300 ease-in-out mt-2 ${
+          isOpen ? "mr-56" : "mr-0"
         }`} // Use transition-all for smooth margin change
         onClick={toggleSidebar}
         aria-label={isOpen ? "Close Sidebar" : "Open Sidebar"}
       >
         <div className="relative">
           {isOpen ? (
-            <HiChevronLeft className="w-6 h-6" />
-          ) : (
             <HiChevronRight className="w-6 h-6" />
+          ) : (
+            <HiChevronLeft className="w-6 h-6" />
           )}
         </div>
       </button>
 
       {/* Sidebar */}
       <div
-        className={`fixed top-20 z-[99999] left-0 h-full bg-white shadow-lg border-r-2 border-r-[#1E3A5F] px-4 py-6 transform ${
-          isOpen ? "translate-x-0 md:translate-x-0" : "md:-translate-x-[81%] -translate-x-full" // Adjust to show 70% on mobile
-        } transition-transform duration-300 ease-in-out w-[70%] md:w-64`} // Set width to 70% on mobile
+        className={`fixed top-20 z-[99999] right-0 h-[80%] rounded-bl-2xl bg-white/40 backdrop-blur-sm shadow-lg border-l-2 border-b-2 border-l-[#1E3A5F] border-b-[#1E3A5F] px-4 py-6 transform ${
+          isOpen ? "translate-x-0" : "translate-x-full md:translate-x-[81%]"
+        } transition-transform duration-300 ease-in-out w-64`} // Set width to 64
       >
         <h2 className="text-lg font-semibold mb-4 border-b-2 border-b-[#1E3A5F] pb-2 flex items-center justify-between">
           HOT BUTTON
